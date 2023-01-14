05bf9cd0d5fa0e55ce2a1ba21bd11b123fc18a41
P1ay2 <66400989+play21947@users.noreply.github.com>
2023-01-15 01:37:28 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import SignUp from './Screens/SignUp'
 import './App.css'
 import Cart from './Screens/Cart'
 import Account from './Screens/Account'
+import Finance from './Screens/Finance'
+import VerifyToken from './Screens/VerifyToken'
+import Reference from './Screens/Reference'
+import EditProfile from './Screens/EditProfile'
 
 const App=()=>{
   return(
@@ -15,6 +19,10 @@ const App=()=>{
       <Route path="/signup" element={<SignUp/>}></Route>
       <Route path='/cart' element={<Cart/>}></Route>
       <Route path='/account' element={<Account/>}></Route>
+      <Route path='/finance' element={<Finance/>}></Route>
+      <Route path='/verify' element={<VerifyToken/>}></Route>
+      <Route path='/reference' element={<Reference/>}></Route>
+      <Route path='/edit_profile' element={<EditProfile/>}></Route>
     </Routes>
   )
 }
