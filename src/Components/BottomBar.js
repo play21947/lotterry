c7296dc772fb04a2dfa5481abe49d0e02d27a03d
P1ay2 <66400989+play21947@users.noreply.github.com>
2023-01-14 23:55:50 +0700

    Add files via upload

--- src/Components/BottomBar.js
+++ src/Components/BottomBar.js
@@ -1,27 +1,71 @@
+import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
+import Swal from "sweetalert2"
+import axios from "axios"
+import { EndPoint } from "../config"
 
 const BottomBar = () => {
 
     let navigate = useNavigate()
 
+    let [quantity, setQuantity] = useState(0)
+
+    const GetAllBought=()=>{
+        return new Promise((resolve, reject)=>{
+            axios.get(`${EndPoint}/api/allbought`,{
+                headers:{
+                    authorization: 'Bearer ' + localStorage.getItem('token')
+                }
+            }).then((res)=>{
+                if(res.data.bad_token){
+                    Swal("Bad Token")
+                }else{
+                    resolve(res.data)
+                }
+            })
+        })
+    }
+
+
+    const initial=async()=>{
+        let ReturnGetAllBought = await GetAllBought()
+
+        // console.log(ReturnGetAllBought)
+
+        setQuantity(ReturnGetAllBought.length)
+    }
+
+    useEffect(()=>{
+        initial()
+    }, [])
+
     return (
         <div className="bottom-bar-container">
-            <div onClick={()=>{
+            <div onClick={() => {
                 navigate("/")
             }} className="bottom-box">
                 <img src="./icons/home.png"></img>
                 <p>หน้าหลัก</p>
             </div>
-            <div onClick={()=>{
+
+            <div onClick={() => {
+                navigate('/finance')
+            }} className="bottom-box">
+                <img src="./icons/money.png"></img>
+                <p>ฝาก / ถอน</p>
+            </div>
+
+            <div onClick={() => {
                 navigate('/cart')
             }} className="bottom-box">
                 <div className="circle">
-                    <p>3</p>
+                    <p>{quantity}</p>
                 </div>
-                <img src="./icons/bag.png"></img>
-                <p>ตะกร้า</p>
+                <img src="./icons/bagpack.png"></img>
+                <p>กระเป๋า</p>
             </div>
-            <div onClick={()=>{
+
+            <div onClick={() => {
                 navigate("/account")
             }} className="bottom-box">
                 <img src="./icons/user.png"></img>
